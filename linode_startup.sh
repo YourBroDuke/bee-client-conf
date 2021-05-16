d35ac3abb46b3e168a633114c992d1fd90989fbe
xiongboyun <xiongboyun@bytedance.com>
2021-05-16 21:05:01 +0800

    adapt for linode

--- linode_startup.sh
+++ linode_startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yum install wget -y
+
 wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.10/bee-clef_0.4.10_amd64.rpm && rpm -i bee-clef_0.4.10_amd64.rpm
 wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.rpm && rpm -i bee_0.5.3_amd64.rpm
 systemctl stop bee
@@ -11,3 +13,4 @@ wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5c
 
 yum install epel-release -y
 yum install jq -y
+yum install screen -y
