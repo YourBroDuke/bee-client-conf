4a3151efee8190936515c628832ca6a310915ac3
xiongboyun <xiongboyun@bytedance.com>
2021-05-16 21:47:20 +0800

    beautify check.sh add chmod +x to check.sh in startup

--- check.sh
+++ check.sh
@@ -14,23 +14,23 @@ FUNC_ADDR='localhost:1633'
 BEE_RES=`curl -s ${FUNC_ADDR}`
 
 if [[ "${BEE_RES}" =~ "Ethereum Swarm Bee" ]]; then
-        echo -e "${Gre} [1] BEE is working properly!${RCol}"
+        echo -e "${Gre}[1] BEE is working properly!${RCol}"
 else
-        echo -e "${Red} [1] BEE is not working properly!${RCol}"
+        echo -e "${Red}[1] BEE is not working properly!${RCol}"
 fi
 
 PEER_NUM=`curl -s http://localhost:1635/peers | jq '.peers | length'`
 if [ ${PEER_NUM} == 0 ]; then
-        echo -e "${Red} [2] BEE haven't conneted to any peer.${RCol}"
+        echo -e "${Red}[2] BEE haven't conneted to any peer.${RCol}"
 else
-        echo -e "${Gre} [2] BEE node has connected to ${PEER_NUM} peers.${RCol}"
+        echo -e "${Gre}[2] BEE node has connected to ${PEER_NUM} peers.${RCol}"
 fi
 
 NEW_CHEQUE=`./cashout.sh | wc -l`
 if [ ${NEW_CHEQUE} == 0 ]; then
-        echo -e "${Lbl} [3] You have no new cheque${RCol}"
+        echo -e "${Lbl}[3] You have no new cheque${RCol}"
 else
-        echo -e "${Yel} [3] Congrats! You have ${NEW_CHEQUE} new cheques!"
+        echo -e "${Yel}[3] Congrats! You have ${NEW_CHEQUE} new cheques!"
         echo -e '--------------------CASHOUT-ALL--------------------${RCol}'
         ./cashout.sh cashout-all
 fi
