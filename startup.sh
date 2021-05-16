159f7d3abeaed0153a704c2a14bfd93db142c7f8
xiongboyun <xiongboyun@bytedance.com>
2021-05-16 20:39:27 +0800

    start.sh

--- startup.sh
+++ startup.sh
@@ -9,3 +9,5 @@ chmod +x ./bee-clef-keys
 ./bee-clef-keys
 wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/b40510f1172b96c21d6d20558ca1e70d26d625c4/cashout.sh
 
+yum install epel-release -y
+yum install jq -y
